9d457bc8830449a796ca2b2ed40bbf35faceb1c2
SnehaVidekar <64341967+SnehaVidekar@users.noreply.github.com>
2025-04-01 13:48:51 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
 
-ingredients_list =st.multiselect(
+'''ingredients_list =st.multiselect(
     'Choose up to 5 ingredients:',my_dataframe, max_selections=5)
 
 if ingredients_list:
@@ -41,7 +41,7 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
-
+  '''
 
 
 
